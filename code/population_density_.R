af19b23033ee538127ac2a77c801cb8e8902a02a
FAYE9393 <faye9393@163.com>
2024-10-10 15:34:18 +0200

    Create population_density_.R

---
+++ code/population_density_.R
@@ -0,0 +1,14 @@
+# How to calculate the density of individuals in a population
+
+# installing the spatstat package
+install.packages("spatstat")
+
+#Recalling the package
+library(spatstat)
+
+# dataset
+bei
+
+plot(bei)
+plot(bei, pch=19)
+plot(bei, pch=19, cex=.5)
