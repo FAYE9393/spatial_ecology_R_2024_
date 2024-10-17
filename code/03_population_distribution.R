d51005565f5c56c60558f5957079f87015281b64
FAYE9393 <faye9393@163.com>
2024-10-17 15:52:54 +0200

    Update 03_population_distribution.R

--- code/03_population_distribution.R
+++ code/03_population_distribution.R
@@ -1,4 +1,26 @@
-install.packages("terra")
-install.package("sdm")
-library(terra)
+# install.packages("sdm")
+# install.packages("terra")
+
 library(sdm)
+library(terra)
+
+file <- system.file("external/species.shp", package="sdm")
+# [1] "/usr/local/lib/R/site-library/sdm/external/species.shp"
+
+rana <- vect(file)
+rana
+
+rana$Occurrence
+
+plot(rana)
+
+pres <- rana[rana$Occurrence==1]
+
+# Exercise: plot in a multiframe the rana dataset beside the pres dataset
+par(mfrow=c(1,2))
+plot(rana)
+plot(pres)
+
+# exercise :plot in a multifram preesences on top of absence 
+ dev.(off)
+plot(pres,col="blue")
