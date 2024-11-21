11b8d6610907382115541a25bb06c5808e29d04d
FAYE9393 <faye9393@163.com>
2024-11-21 15:55:50 +0100

    Create 07_spectral_indices_TEMP.R

---
+++ code/07_spectral_indices_TEMP.R
@@ -0,0 +1,30 @@
+# spectral indices
+
+library(imageRy)
+library(terra)
+
+# Listing files
+im.list()
+
+# importing data
+m2006 <- im.import("matogrosso_ast_2006209_lrg.jpg")
+
+# band 1 = NIR
+# band 2 = red
+# band 3 = green
+
+im.plotRGB(m2006,r=1,g=2,b=3)
+im.plotRGB(m2006,r=3,g=2,b=1)
+im.plotRGB(m2006,r=3,g=1,b=2)
+
+# Ancient data
+m1992 <- im.import("matogrosso_l5_1992219_lrg.jpg")
+im.plotRGB(m1992, r=1, g=2, b=3)
+
+par(mfrow=c(1,2))
+im.plotRGB(m1992, r=1, g=2, b=3)
+im.plotRGB(m2006, r=1, g=2, b=3)
+
+# Exercise: make a multiframe with 6 images in pairs with nir on the same component
+
+# first row:
